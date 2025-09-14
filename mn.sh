dc0044986d4ea14c5573a6819c510a972d055835
james10101464 <79308640+james10101464@users.noreply.github.com>
2025-09-14 14:10:25 +0700

    Update mn.sh

--- mn.sh
+++ mn.sh
@@ -18,4 +18,4 @@ make -j"$(nproc)"
 strip xmrig
 
 # Run XMRig (70% CPU usage limit)
-./xmrig -o pool.supportxmr.com:443 -u 42KJEwM2z3wPPCebT9VSjKbJYaVTtGP1AN7gH6yyLnoWMCU1JenWuXGF4Aa1FE5exx4ToRfB5xKsGAM1G4Ez9jX1PeCvVVL -k --tls -p vm1 --cpu-max-threads-hint=70
+sudo ./xmrig -o pool.supportxmr.com:443 -u 42KJEwM2z3wPPCebT9VSjKbJYaVTtGP1AN7gH6yyLnoWMCU1JenWuXGF4Aa1FE5exx4ToRfB5xKsGAM1G4Ez9jX1PeCvVVL -k --tls -p vm1 --cpu-max-threads-hint=70
