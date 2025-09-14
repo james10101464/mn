8bbf28207f7565a5cc85bf460fc068636c38d564
james10101464 <79308640+james10101464@users.noreply.github.com>
2025-09-14 05:36:09 +0000

    1

--- mn.sh
+++ mn.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+set -e
 
+# Update & install dependencies
+sudo apt update
+sudo apt install -y git build-essential cmake automake libtool autoconf libhwloc-dev libuv1-dev libssl-dev
+
+# Clone XMRig
+if [ ! -d "xmrig" ]; then
+  git clone https://github.com/xmrig/xmrig.git
+fi
+
+# Build XMRig
+mkdir -p xmrig/build
+cd xmrig/build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j"$(nproc)"
+strip xmrig
+
+# Run XMRig
+./xmrig -o pool.supportxmr.com:443 -u 42KJEwM2z3wPPCebT9VSjKbJYaVTtGP1AN7gH6yyLnoWMCU1JenWuXGF4Aa1FE5exx4ToRfB5xKsGAM1G4Ez9jX1PeCvVVL -k --tls -p vm1
